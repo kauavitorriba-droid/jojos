99dfb60744e3efd366c284d07893fc36ffe6c29e
quadrosrenan-byte <quadros.renan@escola.pr.gov.br>
2026-09-16 18:28:16 -0300

    index

---
+++ index.java
@@ -0,0 +1,3 @@
+// Este arquivo cuidará de toda a interatividade e lógica da sua página.
+// Por enquanto, ele só envia uma mensagem escondida para testarmos se está funcionando.
+console.log("O JavaScript está conectado e funcionando perfeitamente!");
